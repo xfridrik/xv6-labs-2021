4fcd0716963fe3c5dbae0fcd63e2768a075dde28
Martin Fridrik <xfridrik@stuba.sk>
2021-09-30 19:11:54 +0200

    feat: implement sleep

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+//waiting user declared ticks
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int ticks;
+  if(argc != 2){
+    fprintf(2,"usage: sleep <ticks>\n");
+    exit(1);
+  }
+
+  ticks = atoi(argv[1]);
+  sleep(ticks);
+
+exit(0);
+}
