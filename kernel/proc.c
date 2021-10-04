77b77004ef50b3582996de9014c4c2ae2f9f376f
Martin Fridrik <xfridrik@stuba.sk>
2021-10-04 20:01:13 +0200

    feat: implement trace system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -292,6 +292,9 @@ fork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // copy parent trace mask
+  np->trace_mask = p->trace_mask;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
